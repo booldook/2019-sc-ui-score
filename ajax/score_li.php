d5b8851adb8e925db5265ce7cd21289c37b32721
booldook <booldook@gmail.com>
2019-04-05 16:06:45 +0900

    Backend 완성

--- ajax/score_li.php
+++ ajax/score_li.php
@@ -1,17 +1,16 @@
 <?
 include '../inc/connect.php';
 
-
 $sql = " SELECT * FROM score ORDER BY id DESC ";
 $result = mysqli_query($conn, $sql);
 
 $str = '{ "student" : [ ';
 while($rs = mysqli_fetch_array($result)) {
 	$str.= '{';
-	$str.= ' "stdname": "'.$stdname.'", ';
-	$str.= ' "kor": "'.$kor.'", ';
-	$str.= ' "eng": "'.$eng.'", ';
-	$str.= ' "math": "'.$math.'" ';
+	$str.= ' "stdname": "'.iconv("UTF-8", "EUC-KR", $rs['stdname']).'", ';
+	$str.= ' "kor": "'.$rs['kor'].'", ';
+	$str.= ' "eng": "'.$rs['eng'].'", ';
+	$str.= ' "math": "'.$rs['math'].'" ';
 	$str.= '},';
 }
 $str = substr($str, 0, -1);
