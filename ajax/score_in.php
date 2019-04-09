ea6c6ed4dc66a9de8a65fb195eb0f38175fd5b33
booldook <booldook@gmail.com>
2019-04-09 12:04:29 +0900

    delete/update

--- ajax/score_in.php
+++ ajax/score_in.php
@@ -10,8 +10,6 @@ $sql = " INSERT INTO score SET kor=$kor, eng=$eng, math=$math, stdname='$stdname
 
 mysqli_query($conn, $sql);
 
-$id = mysqli_insert_id($conn);
-
 $str = '{ "code": 200 }';
 echo $str;
 ?>
--- js/score_ajax.js
+++ js/score_ajax.js
@@ -18,6 +18,10 @@ function getScore(res) {
 		html += '<td>'+std[i].eng+'점</td>';
 		html += '<td>'+std[i].math+'점</td>';
 		html += '<td>'+avg+'점</td>';
+		html += '<td>';
+		html += '<i onclick="delData(this);" class="fas fa-trash-alt pointer" data-id="'+std[i].id+'"></i> ';
+		html += '<i onclick="upData(this);" class="fas fa-wrench pointer" data-id="'+std[i].id+'"></i> ';
+		html += '</td>';
 		html += '</tr>';
 		$(".score_tb tbody").append(html);
 	}
@@ -49,27 +53,26 @@ data: {},								//자바스크립트 객체  => 보낼 데이터
 success: function(){},	//함수								=> 함수를 보내고, 통신이 성공하는 시점에서 실행시킨다.
 error: function(){}			//함수								=> 함수를 보내고, 통신이 실패하는 시점에서 실행시킨다.
 */
-var option = {
-	url: "./score_in.php",
-	type: "post",
-	dataType: "json",
-	data: {
-		stdname: $("#stdname").val(),
-		kor: $("#kor").val(),
-		eng: $("#eng").val(),
-		math: $("#math").val()
-	},
-	success: function(res){
-		if(res.code == 200) getList();
-		else alert("데이터 처리에 문제가 있습니다.\n잠시후 다시 시도해 주세요.");
-	},
-	error: function(xhr){
-		console.log(xhr);
-		alert("통신에 문제가 있습니다.\n잠시후 다시 시도해 주세요.");
-	}
-};
 $("#bt_save").click(function(){
+	var option = {
+		url: "score_in.php",
+		type: "post",
+		dataType: "json",
+		data: {
+			stdname: $("#stdname").val(),
+			kor: $("#kor").val(),
+			eng: $("#eng").val(),
+			math: $("#math").val()
+		},
+		success: function(res){
+			console.log(res);
+			if(res.code == 200) getList();
+			else alert("데이터 처리에 문제가 있습니다.\n잠시후 다시 시도해 주세요.");
+		},
+		error: err
+	};
 	$.ajax(option);
 });
+
 
 
