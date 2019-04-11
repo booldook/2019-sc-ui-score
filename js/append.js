d85fca3290f2e6993a6705492111185fc3b3b3a6
booldook <booldook@gmail.com>
2019-04-11 09:49:42 +0900

    append

--- js/append.js
+++ js/append.js
@@ -1,3 +1,25 @@
+/*
+1. $("객체")		2. '<div>A</div>'			3. "id"			4. "idName"
+5. "className"			6. {"display":"none"}			7. "display", "none"
+
+append()						=>	$("객체").append('<div>A</div>');
+appendTo()					=>	$('<div>A</div>').appendTo($("객체"));
+prepend()						=>	$("객체").prepend('<div>A</div>');
+prependTo()					=>	$('<div>A</div>').prependTo($("객체"));
+html()							=>	$("객체").html('<div>A</div>');
+empty()							=>	$("객체").empty();
+remove()						=>	$("객체").remove();
+css()								=>	$("객체").css({"display":"none", "opacity":0.5});
+												$("객체").css("display", "none");
+												var v = $("객체").css("display");
+addClass()					=>	$("객체").addClass("className");
+removeClass()				=>	$("객체").removeClass("className");
+attr()							=>	var id = $("객체").attr("id");
+												$("객체").attr("id", "idName");
+data()							=>	var id = $("객체").data("id");
+*/
+
+
 $(".box").click(function(){
 	alert("Hi~");
 });
@@ -13,17 +35,17 @@ $("#bt_append2").click(function(){
 	});
 });
 $("#bt_prepend").click(function(){
-	
+	$(".wrap").prepend('<div class="box">prepend()</div>');
 });
 $("#bt_prepend2").click(function(){
-	
+	$('<div class="box">prependTo()</div>').prependTo($(".wrap"));
 });
 $("#bt_html").click(function(){
-	
+	$(".wrap").html('<div class="box">html()</div>');
 });
 $("#bt_empty").click(function(){
-	
+	$(".wrap").empty();
 });
 $("#bt_remove").click(function(){
-	
+	$(".wrap").remove();
 });
